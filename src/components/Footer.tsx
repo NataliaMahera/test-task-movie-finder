4980332674d3eace06fddca331d7abd8e554e1e2
NataliaMahera <magera.nataliia@gmail.com>
2024-10-17 09:00:58 +0200

    updatae: header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
-    return (
-        <footer>Footer</footer>
-    );
+  return (
+    <footer className="p-8 border-t-[1px]">
+      <div className="text-center">
+        {' '}
+        &#169; Movie Finder 2024 | Developed by{' '}
+        <Link
+          className="font-semibold"
+          to="https://github.com/NataliaMahera"
+          target="_blank"
+        >
+          Natalia Mahera
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,42 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+import Logo from './Logo';
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
-    <header>
-      <nav>
-        <ul>
+    <header className="px-8 bg-gradient-to-r from-blue-500 to-purple-600 shadow-lg flex justify-center items-center">
+      <Logo/>
+      <nav className="max-w-7xl mx-auto px-6 py-8">
+        <ul className="flex justify-center space-x-10">
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                clsx(
+                  "text-sm sm:text-2xl font-semibold transition-all duration-300 ease-in-out",
+                  isActive
+                    ? "text-yellow-300 font-bold border-b-2 border-yellow-300"
+                    : "text-white hover:text-yellow-300"
+                )
+              }
+            >
+              Watch Guide
+            </NavLink>
           </li>
           <li>
-            <Link to="/favorites">Favorites</Link>
+            <NavLink
+              to="/favorites"
+              className={({ isActive }) =>
+                clsx(
+                  "text-sm sm:text-2xl font-semibold transition-all duration-300 ease-in-out",
+                  isActive
+                    ? "text-yellow-300 font-bold border-b-2 border-yellow-300 pointer-events-none"
+                    : "text-white hover:text-yellow-300"
+                )
+              }
+            >
+              Favorites
+            </NavLink>
           </li>
         </ul>
       </nav>
