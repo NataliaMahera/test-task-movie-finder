f43514b6a8b3ac2b85ebaca4f786414e5e0ff367
NataliaMahera <magera.nataliia@gmail.com>
2024-10-17 09:02:35 +0200

    fix: del strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import App from './App.tsx';
 import './index.css';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
