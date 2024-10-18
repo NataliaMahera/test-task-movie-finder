bd99a764536bb7c78ea9e2f177d1b580122c410f
NataliaMahera <magera.nataliia@gmail.com>
2024-10-18 16:49:25 +0200

    fix: loader style

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,7 @@
 import { ThreeDots } from 'react-loader-spinner';
 export const Loader = () => {
   return (
-    <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+    <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[80vh]'>
       <ThreeDots
         height="80"
         width="80"
@@ -15,3 +15,5 @@ export const Loader = () => {
     </div>
   );
 };
+
+// className="flex justify-center items-center h-[80vh]"
