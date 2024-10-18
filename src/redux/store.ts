ea8fba97eb2f9c15e5309d365f68e54a0862dba1
NataliaMahera <magera.nataliia@gmail.com>
2024-10-19 00:38:27 +0200

    update: store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
-import favoritesReducer from './favorites/favoritesSlice';
+import favoritesReducer, { FavoritesState } from './favorites/favoritesSlice';
 import moviesReducer from './movies/moviesSlice'
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import {
   persistStore,
-  // FLUSH,
-  // REHYDRATE,
-  // PAUSE,
-  // PERSIST,
-  // PURGE,
-  // REGISTER,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from 'redux-persist';
+import { PersistPartial } from 'redux-persist/lib/persistReducer';
 
 const favoritesConfig  = {
   key: 'favorites',
@@ -21,18 +22,17 @@ const favoritesConfig  = {
 export const store = configureStore({
   reducer: { 
     movies: moviesReducer,
-    favorites: persistReducer(favoritesConfig ,favoritesReducer)
+    favorites: persistReducer<FavoritesState & PersistPartial>(favoritesConfig, favoritesReducer),
 },
-// middleware: (getDefaultMiddleware) =>
-//   getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
+
+middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 export const persistor = persistStore(store);
