210fe23ab433456c3bceacc9c6ecead9959b704f
NataliaMahera <magera.nataliia@gmail.com>
2024-10-18 16:53:36 +0200

    create: reducer for favorites

--- src/redux/favorites/favoritesSlice.ts
+++ src/redux/favorites/favoritesSlice.ts
@@ -1,34 +1,30 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import { Movie } from '../movies/movies.types';
 
 export interface FavoritesState {
-  value: number
+  favoriteItems: Movie[],
 }
 
 const initialState: FavoritesState = {
-  value: 0,
+  favoriteItems: [],
 }
 
-export const FavoritesSlice = createSlice({
+export const favoritesSlice = createSlice({
   name: 'favorites',
   initialState,
   reducers: {
-    increment: (state) => {
-        // Redux Toolkit allows us to write "mutating" logic in reducers. It
-        // doesn't actually mutate the state because it uses the Immer library,
-        // which detects changes to a "draft state" and produces a brand new
-        // immutable state based off those changes
-        state.value += 1
-      },
-      decrement: (state) => {
-        state.value -= 1
-      },
-      incrementByAmount: (state, action: PayloadAction<number>) => {
-        state.value += action.payload
-      },
+    addToFavorites: (state, { payload }) => {
+      state.favoriteItems.push(payload);
+    },
+    deleteFromFavorites: (state, { payload }) => {
+      state.favoriteItems = state.favoriteItems.filter(
+        ({ id }) => id !== payload.id
+      );
+    },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const {increment, decrement, incrementByAmount} = FavoritesSlice.actions
+export const {addToFavorites, deleteFromFavorites} = favoritesSlice.actions
 
-export default FavoritesSlice.reducer
+export default favoritesSlice.reducer
