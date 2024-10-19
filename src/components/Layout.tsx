7cd2df2c1844998e605abdfde06dbb7c7abb67b1
NataliaMahera <magera.nataliia@gmail.com>
2024-10-19 12:28:46 +0200

    fix: container style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
-      <main className="flex-grow min-w-full w-[100vw] min-h-[70vh]">{children}</main>
+      <main className="flex-grow w-[100vw] overflow-x-hidden min-h-[70vh]">{children}</main>
       <Footer />
     </>
   );
