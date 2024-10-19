198cf97e2dade8ccd1fce1ec8abf9b1a8299256d
NataliaMahera <magera.nataliia@gmail.com>
2024-10-19 13:15:22 +0200

    update: footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="p-8 border-t-[1px]">
+    <footer className="p-8 border-t-[1px] border-gray-600">
       <div className="text-center">
         {' '}
         &#169; Movie Finder 2024 | Developed by{' '}
         <Link
-          className="font-semibold"
+          className="font-semibold transition-all duration-300 ease-in-out hover:text-blue-400 hover:underline "
           to="https://github.com/NataliaMahera"
           target="_blank"
         >
