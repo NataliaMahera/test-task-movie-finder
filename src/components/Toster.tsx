bfb50690ea4f21cae4eef6599c40db9581214043
NataliaMahera <magera.nataliia@gmail.com>
2024-10-31 19:40:55 +0100

    add: toster component

---
+++ src/components/Toster.tsx
@@ -0,0 +1,15 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { Flip } from 'react-toastify';
+import { ToastOptions } from 'react-toastify';
+
+export const styleToastify: ToastOptions = {
+  position: 'bottom-right',
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored',
+  transition: Flip,
+};
