f58b56216077edbd2b5fe5a7187752bf4dee43c6
NataliaMahera <magera.nataliia@gmail.com>
2024-11-01 13:25:53 +0100

    fix: del clsx and update styles conditions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import clsx from 'clsx';
 import { RootState } from '../redux/store';
 import Logo from './Logo';
 import AuthNav from './Profile/AuthNav';
@@ -8,7 +7,7 @@ import UserMenu from './Profile/UserMenu';
 
 const Header: React.FC = () => {
   const { isLoggedIn } = useSelector((state: RootState) => state.auth);
-  
+
   return (
     <header className="px-8 bg-gradient-to-r from-blue-500 to-purple-600 shadow-lg w-full flex justify-center items-center">
       <Logo />
@@ -18,12 +17,11 @@ const Header: React.FC = () => {
             <NavLink
               to="/"
               className={({ isActive }) =>
-                clsx(
-                  'text-sm sm:text-2xl font-semibold transition-all duration-300 ease-in-out',
-                  isActive
-                    ? 'text-yellow-300 font-bold border-b-2 border-yellow-300'
+                `text-sm sm:text-2xl font-semibold transition-all duration-300 ease-in-out 
+              ${ isActive
+                    ? 'text-yellow-300 font-bold border-b-2 border-yellow-300 pointer-events-none'
                     : 'text-white hover:text-yellow-300'
-                )
+                }`
               }
             >
               Watch Guide
@@ -34,12 +32,11 @@ const Header: React.FC = () => {
               <NavLink
                 to="/favorites"
                 className={({ isActive }) =>
-                  clsx(
-                    'text-sm sm:text-2xl font-semibold transition-all duration-300 ease-in-out',
-                    isActive
+                  `text-sm sm:text-2xl font-semibold transition-all duration-300 ease-in-out 
+                ${ isActive
                       ? 'text-yellow-300 font-bold border-b-2 border-yellow-300 pointer-events-none'
                       : 'text-white hover:text-yellow-300'
-                  )
+                  }`
                 }
               >
                 Favorites
--- src/components/ScrollUpBtn.tsx
+++ src/components/ScrollUpBtn.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import React, { useEffect, useState } from 'react';
 import { AiOutlineArrowUp } from 'react-icons/ai';
 
@@ -30,13 +29,8 @@ const ScrollUpBtn: React.FC = () => {
   return (
     <button
       onClick={scrollToTop}
-      className={clsx(
-        'fixed bottom-16 right-5 transition-all duration-300 ease-in-out z-30 rounded-full p-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white hover:from-blue-600 hover:to-blue-800 hover:scale-105 hover:shadow-xl',
-        {
-          'opacity-100 visible translate-x-0': isVisible,
-          'opacity-0 invisible translate-x-full': !isVisible,
-        }
-      )}
+      className={`fixed bottom-16 right-5 transition-all duration-300 ease-in-out z-30 rounded-full p-3 bg-gradient-to-r from-blue-500 to-blue-700 text-white hover:from-blue-600 hover:to-blue-800 hover:scale-105 hover:shadow-xl' 
+      ${ isVisible ? 'opacity-100 visible translate-x-0' : 'opacity-0 invisible translate-x-full'}`}
       aria-label="Scroll to top"
     >
       <AiOutlineArrowUp size={24} />
