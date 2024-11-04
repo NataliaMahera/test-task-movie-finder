998e843e00a45759b14953710831c50ef62db625
NataliaMahera <magera.nataliia@gmail.com>
2024-11-04 17:29:04 +0100

    add: observer component

---
+++ src/components/InfiniteScroll.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from 'react';
+
+interface InfiniteScrollProps {
+  currentPage: number;
+  totalPages: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  isLoading: boolean;
+}
+
+const InfiniteScroll: React.FC<InfiniteScrollProps> = ({ currentPage, totalPages, setCurrentPage, isLoading }) => {
+  const observerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+
+    const callbackFunction = (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting && !isLoading) {
+        setCurrentPage((prevPage) => (prevPage < totalPages ? prevPage + 1 : prevPage));
+      }
+    };
+
+    const options = {
+      root: null,
+      rootMargin: '100px',
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(callbackFunction, options);
+    const currentRef = observerRef.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [currentPage, isLoading, setCurrentPage, totalPages]);
+
+  return <div ref={observerRef} />;
+};
+
+export default InfiniteScroll;
