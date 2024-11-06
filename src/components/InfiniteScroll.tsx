ff729250683f9018abffdded9af700bceb25683e
NataliaMahera <magera.nataliia@gmail.com>
2024-11-06 14:21:21 +0100

    update: scroll

--- src/components/InfiniteScroll.tsx
+++ src/components/InfiniteScroll.tsx
@@ -1,56 +1,69 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { Loader } from './Loader';
 
 interface InfiniteScrollProps {
   currentPage: number;
   totalPages: number;
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
   isLoading: boolean;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  topObserverRef: React.RefObject<HTMLDivElement>;
+  bottomObserverRef: React.RefObject<HTMLDivElement>;
 }
 
 const InfiniteScroll: React.FC<InfiniteScrollProps> = ({
-  currentPage,
   totalPages,
   setCurrentPage,
   isLoading,
+  currentPage,
+  topObserverRef,
+  bottomObserverRef,
 }) => {
-  const observerRef = useRef<HTMLDivElement | null>(null);
-
   useEffect(() => {
-    const callbackFunction = (entries: IntersectionObserverEntry[]) => {
-      if (entries[0].isIntersecting && !isLoading) {
-        setCurrentPage((prevPage) =>
-          prevPage < totalPages ? prevPage + 1 : prevPage
-        );
+    const loadNextPage = (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting && !isLoading && currentPage < totalPages) {
+        setCurrentPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    const loadPrevPage = (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting && !isLoading && currentPage > 1) {
+        setCurrentPage((prevPage) => prevPage - 1);
       }
     };
 
     const options = {
       root: null,
-      rootMargin: '100px',
-      threshold: 0.5,
+      rootMargin: '50px',
+      threshold: 0.25,
     };
 
-    const observer = new IntersectionObserver(callbackFunction, options);
-    const currentRef = observerRef.current;
+    const bottomObserver = new IntersectionObserver(loadNextPage, options);
+    const topObserver = new IntersectionObserver(loadPrevPage, options);
 
-    if (currentRef) {
-      observer.observe(currentRef);
+    const topRef = topObserverRef.current;
+    const bottomRef = bottomObserverRef.current;
+
+    if (bottomRef) {
+      bottomObserver.observe(bottomRef);
+    }
+    if (topRef) {
+      topObserver.observe(topRef);
     }
 
     return () => {
-      if (currentRef) {
-        observer.unobserve(currentRef);
-      }
+      if (bottomRef) bottomObserver.unobserve(bottomRef);
+      if (topRef) topObserver.unobserve(topRef);
     };
-  }, [currentPage, isLoading, setCurrentPage, totalPages]);
+  }, [
+    bottomObserverRef,
+    currentPage,
+    isLoading,
+    setCurrentPage,
+    topObserverRef,
+    totalPages,
+  ]);
 
-  return (
-    <div>
-      <div ref={observerRef} />
-      {isLoading && <Loader fixed={false} />}
-    </div>
-  );
+  return <>{isLoading && <Loader fixed={false} />}</>;
 };
 
 export default InfiniteScroll;
