f3d9d7c079fe88072638453ff8322f541ff473e2
NataliaMahera <magera.nataliia@gmail.com>
2024-11-06 11:56:06 +0100

    update: add nuqs adapter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { NuqsAdapter } from 'nuqs/adapters/react';
 import { createRoot } from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store.ts';
@@ -9,12 +10,14 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate loading={null} persistor={persistor}>
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <NuqsAdapter>
           <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
+        </NuqsAdapter>
+      </PersistGate>
+    </BrowserRouter>
+  </Provider>
   // </StrictMode>
 );
